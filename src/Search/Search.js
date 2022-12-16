ccf84badf192c1c08b0268b4497df924786b1815
Muhammad Arif Razmi <82495624+marifrazmi@users.noreply.github.com>
2022-12-16 19:40:21 +0700

    Update Search.js

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -13,7 +13,7 @@ const Search = () => {
     const doSearch = (e) => {
       axios
         .get(
-          "https://my-json-server.typicode.com/marifrazmi/SayangAnakdb/toys?q=" + color
+          "https://my-json-server.typicode.com/marifrazmi/SayangAnakdb/toys?color like=" + color
         )
         .then((results) => {
           setData(results.data);
